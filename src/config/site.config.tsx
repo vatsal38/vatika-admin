81df97880a06bb49149d8adae398737a37991b39
Nikunj Mavani <nikunjmavani80@gmail.com>
2024-06-19 19:23:30 +0530

    Completed

--- src/config/site.config.tsx
+++ src/config/site.config.tsx
@@ -24,17 +24,16 @@ export const metaObject = (
   openGraph?: OpenGraph,
   description: string = siteConfig.description
 ): Metadata => {
-  console.log('title::: ', title);
   return {
     title: title ? `${title}` : siteConfig.title,
     description,
     openGraph: openGraph ?? {
       title: title ? `${title}` : title,
       description,
-      url: 'https://isomorphic-furyroad.vercel.app',
-      siteName: 'Isomorphic Furyroad', // https://developers.google.com/search/docs/appearance/site-names
+      url: 'https://vatika-admin.vercel.app',
+      siteName: 'Vatika Green', // https://developers.google.com/search/docs/appearance/site-names
       images: {
-        url: 'https://s3.amazonaws.com/redqteam.com/isomorphic-furyroad/itemdep/isobanner.png',
+        url: 'https://vatika-ui.vercel.app/_next/image?url=https%3A%2F%2Fdev-vatikagreen-s3.s3.amazonaws.com%2F1717058269410-646197974.png%3FAWSAccessKeyId%3DAKIAQ3EGTZ6FIOOHWVP2%26Expires%3D1718823121%26Signature%3DPPSgbleknSWjPateW7rOqJ2v9rk%253D&w=1920&q=75',
         width: 1200,
         height: 630,
       },
