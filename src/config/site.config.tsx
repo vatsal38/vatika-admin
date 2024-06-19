c9f754672fa9aabcca8f077e3de2a7005a1041ae
Nikunj Mavani <nikunjmavani80@gmail.com>
2024-06-19 20:01:53 +0530

    Finish

--- src/config/site.config.tsx
+++ src/config/site.config.tsx
@@ -2,6 +2,8 @@ import { Metadata } from 'next';
 import logoImg from '@public/logo.svg';
 import { LAYOUT_OPTIONS } from '@/config/enums';
 import logoIconImg from '@public/logo-short.svg';
+import logo from '@public/logo-vatika.png';
+import logoImage from '@public/vatika.png';
 import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types';
 
 enum MODE {
@@ -12,7 +14,7 @@ enum MODE {
 export const siteConfig = {
   title: 'Vatika',
   description: `Vatika green`,
-  logo: logoImg,
+  logo: logo,
   icon: logoIconImg,
   mode: MODE.LIGHT,
   layout: LAYOUT_OPTIONS.HYDROGEN,
@@ -27,18 +29,20 @@ export const metaObject = (
   return {
     title: title ? `${title}` : siteConfig.title,
     description,
-    openGraph: openGraph ?? {
-      title: title ? `${title}` : title,
-      description,
-      url: 'https://vatika-admin.vercel.app',
-      siteName: 'Vatika Green', // https://developers.google.com/search/docs/appearance/site-names
-      images: {
-        url: '/vatika.png',
-        width: 1200,
-        height: 630,
-      },
-      locale: 'en_US',
-      type: 'website',
-    },
+    openGraph:
+      openGraph ??
+      ({
+        title: title ? `${title}` : title,
+        description,
+        url: logoImage,
+        siteName: 'Vatika Green', // https://developers.google.com/search/docs/appearance/site-names
+        images: {
+          url: '/vatika.png',
+          width: 1200,
+          height: 630,
+        },
+        locale: 'en_US',
+        type: 'website',
+      } as any),
   };
 };
